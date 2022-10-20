4e854427f593bc2053de08bfa7fdfebf664e2c79
dingxq <xiaoqiding@126.com>
2022-10-20 08:53:23 +0800

    新增Header组件引用

---
+++ vue_v1/vue/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="height: 50px;line-height: 50px;display: flex;border-bottom: 1px solid #cccccc" >
+      <div style="width: 200px">后台管理系统</div>
+      <div style="flex:1"></div>
+      <div style="width: 100px">张三</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
