2fa148b2375df1cfb31cbce194d538756e1c31cb
dingxq <xiaoqiding@126.com>
2022-10-22 11:03:20 +0800

    修改header样式

--- vue_v1/vue/src/components/Header.vue
+++ vue_v1/vue/src/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
     <div style="height: 50px;line-height: 50px;display: flex;border-bottom: 1px solid #cccccc" >
-      <div style="width: 200px">后台管理系统</div>
+      <div style="width: 200px;">后台管理系统</div>
       <div style="flex:1"></div>
-      <div style="width: 100px">张三</div>
+      <div style="width: 100px">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            宇智波<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+      </div>
     </div>
 </template>
 
