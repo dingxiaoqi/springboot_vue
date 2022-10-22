80f604d24db90f26cd4a6899ac5473dd228e3688
dingxq32842 <xiaoqiding@126.com>
2022-10-22 19:45:52 +0800

    设置侧边栏

--- vue_v1/vue/src/App.vue
+++ vue_v1/vue/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
+<!--    头部-->
     <Header></Header>
-    <el-button type="primary">按钮测试</el-button>
-    <router-view/>
+<!--    主体-->
+    <div style="display: flex">
+<!--      侧边栏-->
+      <Aside/>
+<!--      内容-->
+      <router-view style="flex: 1"/>
+    </div>
   </div>
 
 </template>
 
 <script>
   import Header from "@/components/Header";
+  import Aside from "@/components/Aside";
 
   export default {
     name:"Layout",
     components:{
-      Header
+      Header,
+      Aside
     }
   }
 </script>
---
+++ vue_v1/vue/src/components/Aside.vue
@@ -0,0 +1,49 @@
+<template>
+      <el-menu
+          style="width:200px;min-height: calc(100vh - 50px )"
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-submenu index="1-4">
+            <template slot="title">选项1</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Aside",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
