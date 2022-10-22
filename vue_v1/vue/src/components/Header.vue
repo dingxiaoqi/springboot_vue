9ad65fa172dac682c0001ba22b094da8cecd1546
dingxq32842 <xiaoqiding@126.com>
2022-10-22 18:56:00 +0800

    头部样式修改

--- vue_v1/vue/src/components/Header.vue
+++ vue_v1/vue/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="height: 50px;line-height: 50px;display: flex;border-bottom: 1px solid #cccccc" >
-      <div style="width: 200px;">后台管理系统</div>
+      <div style="width: 200px;padding-left: 30px;font-weight: bold;color:dodgerblue">后台管理系统</div>
       <div style="flex:1"></div>
       <div style="width: 100px">
         <el-dropdown>
